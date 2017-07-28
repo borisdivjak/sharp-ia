5a1f5988de6c628ef5a88b3153732cbc5fb5c978
Boris Divjak <boris@divjak.net>
2017-07-28 14:40:45 +0100

    disable browser back on scroll

---
+++ assets/js/disable-scroll-back.js
@@ -0,0 +1,22 @@
+// prevents browser from triggering 'back' on scrolling left too far (e.g. on Chrome on OsX)
+// when #scrollable scrolled all the way to the left
+// requires jquery
+
+
+function preventScrollLeft(e) {
+	if (e.originalEvent.wheelDeltaX < 0) $(e.delegateTarget).off('wheel');
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+$(function () {
+	$(this).on('wheel', preventScrollLeft);
+	$('#scrollable').scroll(function() {
+		if ($(this).scrollLeft() == 0) {
+			$(this).on('wheel', preventScrollLeft);
+		}
+	});
+
+});
