ce864017699678ade4974b78bdb62da7b7487df5
Boris Divjak <boris@divjak.net>
2017-07-29 10:42:43 +0100

    Modal loading via ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,13 +10,18 @@ $(function () {
 function loadDone(svg, error) { 
 	$('svg a path').on('mouseover', function() { $(this).attr('fill', '#000000').attr('fill-opacity', '0.1'); });
 	$('svg a path').on('mouseout', 	function() { $(this).attr('fill', '#000000').attr('fill-opacity', '0'); });
+
+	// open modal and load content from html
 	$('svg a').on('click', function(e) {
 		e.preventDefault();
-		$('#modal-background').css('display', 'block');
-		$($(this).attr('xlink:href')+'.modal').css('display', 'block'); // note - xlink:href works for links from google slides
+		$('.modal-background').css('display', 'block');
+		$('.modal').css('display', 'block'); 
+		$('.modal-content').load( $(this).attr('xlink:href').slice(1) + '.html' ); // note - xlink:href works for links from google slides
 	});
-	$('.modal .close-modal, #modal-background').on('click', function(e) {
-		$('#modal-background, .modal').css('display', 'none');
+
+	// close modal
+	$('.modal .close-modal, .modal-background').on('click', function(e) {
+		$('.modal-background, .modal').css('display', 'none');
+		$('.modal-content').html('');
 	});
 }
-	
