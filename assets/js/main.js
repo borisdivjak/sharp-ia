d289d7e4878893e34b68397a6e09e08a5d3eb903
Boris Divjak <boris@divjak.net>
2017-09-03 01:14:39 +0100

    remove home page SVG

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,10 @@
-var svgBaseWidth = 0;
-var svgBaseHeight = 0;
-
 $(function () {
     // Disable caching of AJAX responses
 	$.ajaxSetup ({ cache: false });
 
-	// Link directly to google slides (make sure link to presentation is open to everyone) using the following format:
-	// https:\/\/docs.google.com/presentation/d/<PRESENTATION_ID>/export/<FORMAT>?pageid=<PAGE_ID>
-	$('.svgholder').svg({loadURL: 'https://docs.google.com/presentation/d/1aoYC6LcCzFdC8bTmii1Ciz_QFxmwXov24x7Z9dphnaI/export/svg?pageid=g24d4306262_0_12', onLoad: loadDone });	
-//	$('.svgholder').svg({loadURL: 'https://docs.google.com/presentation/d/1iZeU-UGoDyJSDpJ5Za7PDcPkp99XkUl9bxiN4SEx8kA/export/svg?pageid=g24d4306262_0_12', onLoad: loadDone });	
 
 	$(window).on('resize', function() {
-		$('.svgholder svg').attr('width', $('.svgholder').width());
-		
-		// on narrow screens reduce height 
-		if ($('.svgholder').width() < svgBaseWidth) {
-			$('.svgholder svg').attr('height', $('.svgholder').width() / svgBaseWidth * svgBaseHeight);			
-		}
-		// on wider screens set effective max height for svg based on original dimesnions
-		else {
-			$('.svgholder svg').attr('height', svgBaseHeight);
-		}
-		
+				
 		// keep video ratio at 16:9
 		var iframeWidth = $('.modal iframe').css('width');
 		var iframeHeight = 0;
@@ -29,17 +12,6 @@ $(function () {
 		$('.modal iframe').css('height', iframeHeight);
 	});
 	
-	$(window).resize();
-}); 
- 
-function loadDone(svg, error) { 
-	svgBaseWidth = svg._svg.viewBox.baseVal.width;
-	svgBaseHeight = svg._svg.viewBox.baseVal.height;
-	$(window).resize();
-
-	$('svg a path').on('mouseover', function() { $(this).attr('fill', '#000000').attr('fill-opacity', '0.1'); });
-	$('svg a path').on('mouseout', 	function() { $(this).attr('fill', '#000000').attr('fill-opacity', '0'); });
-
 	// open modal and load content from html
 	$('svg a').on('click', function(e) {
 		e.preventDefault();
@@ -67,4 +39,6 @@ function loadDone(svg, error) {
 	$('.modal').on('click', function(e) {
 		e.stopPropagation();
 	});
-}
+
+	$(window).resize();
+}); 
