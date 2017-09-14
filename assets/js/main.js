2c46654c7263dfa5d5d988bd7fbb6f309ea3be4f
Boris Divjak <boris@divjak.net>
2017-09-14 23:49:58 +0100

    add animations when home items become visible

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
-$(function () {
+$(function() {
 	var bodyclass = '';
+
+	function animateVisible() {
+		$('.animate-when-visible').each(function(index, element) {
+			if ($(element).visible(true)) {
+				setTimeout(function() {
+					console.log('index: ' + index);
+					$(element).addClass('animating-now').removeClass('animate-when-visible')
+						.on('transitionend', function() {
+							$(this).removeClass('animating-now');
+						});
+				}, 50 * (index + Math.random())); // load earlier elements first, but add a bit of random timing for a more natural effect
+			}
+		});		
+	}
 
 	if (!Modernizr.touchevents) {
 		bodyclass = 'hover-animation ';
@@ -16,10 +30,14 @@ $(function () {
 		var iframeHeight = 0;
 		if (iframeWidth) iframeHeight = Math.floor(parseInt(iframeWidth) * 0.5625) + 'px';
 		$('iframe').css('height', iframeHeight);
+		animateVisible();
 	});
 	
 	$(window).resize();	
-			
+
+
+	$(window).on('scroll', animateVisible);
+				
 	
 	// ROUTING: sammy is the routing function	
 	$.sammy(function() {
@@ -28,6 +46,8 @@ $(function () {
 			$('main.page-content').load( 'home-content.html', function() {
 				$('body').attr('class', bodyclass + 'home');
 				$(window).scrollTop(0);
+				$('.home-item').addClass('animate-when-visible');
+				animateVisible();
 			});
 		});
 
